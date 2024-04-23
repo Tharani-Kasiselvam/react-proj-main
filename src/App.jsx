f72968c924ddfdafe4b038924f1580455dda85ba
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-04-23 17:23:56 +0530

    day4 Exercise - increment/decrement/reset - State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import Hello from "./components/Hello"
-const App = () => {
-  const usrname = "Tharani"
-  return (
-    <div>
-      <Hello user = {usrname}/>
-    </div>
-  )
+import { Component } from 'react'
+import Counter from './components/Counter'
+
+export class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {counter:0}
+  }
+
+    handleIncre = () =>{
+      this.setState({
+        counter: this.state.counter + 1
+    })
+    }
+
+    handleDecre = () =>{
+      this.setState({
+        counter: this.state.counter - 1
+      })
+      }
+
+      handleRst = () => {
+        this.setState({
+          counter: 0
+        })
+      }
+
+  render() {
+    return (
+      <div>
+      <div>
+        <Counter counter={this.state.counter} />
+      </div>
+        <div><button onClick={this.handleIncre}>Increment</button>
+            <button onClick={this.handleDecre}>Decrement</button>
+            <button onClick={this.handleRst}>Reset</button> 
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/Greet.jsx
+++
@@ -1,6 +0,0 @@
-const Greet = (props) => {
-    return (
-      <p>Hi {props.name} {props.lname}! Welcome to our Application </p>
-    )
-  }  
-  export default Greet
