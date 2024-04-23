fc5358f1d1e82811e0e9793d19a7a5436c9b354e
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-04-23 08:40:16 +0530

    class comp - multip comp - passing props

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,21 @@
-const Hello = (props) => {
-    return(
-      <div>
-        <h1>Hello World</h1>
-        <p>This is a simple <b>Component</b></p>
-        <p>by {props.user}</p>
-      </div>
+// const Hello = (props) => {
+//     return(
+//       <div>
+//         <h1>Hello World</h1>
+//         <p>This is a simple <b>Component</b></p>
+//         <p>by {props.user}</p>
+//       </div>
+//     )
+// }
+// export default Hello  
+import { Component } from 'react'
+
+export class Hello extends Component {
+  render() {
+    return (
+      <p>Helo {this.props.user}</p>
     )
+  }
 }
-export default Hello  
+
+export default Hello
