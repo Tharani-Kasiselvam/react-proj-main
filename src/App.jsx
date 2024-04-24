33cfaf9b72e3d2e644fd9a39fe21aa8450916d59
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-04-24 07:36:53 +0530

    Hooks-UseState-3btns-setCounter

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,33 @@ import { useState } from "react"
 
 const App = () => {
 
-  const[counter,setCounter]=useState(0)
+  const[plusCount,setPluscnt]=useState(0)
+  const[minCount,setMincnt]=useState(0)
+  const[zeCount,setZecnt]=useState(0)
 
-  const hndlPlus = () =>{
-      setCounter(counter+1)
-  }
 
-  const hndlMinus = () =>{
-    setCounter(counter-1)
-} 
 
-const hndlZe = () =>{
-  setCounter(0)
-}
+//   const hndlPlus = () =>{
+//       setCounter(counter+1)
+//   }
+
+//   const hndlMinus = () =>{
+//     setCounter(counter-1)
+// } 
+
+// const hndlZe = () =>{
+//   setCounter(0)
+// }
 
   return (
     <div>
-    <div>Counter: {counter}</div>
+      <div>Plus count: {plusCount}</div>
+      <div>Minus count: {minCount}</div>
+      <div>Zero count: {zeCount}</div>
     <div>
-      <button onClick={hndlPlus}>Plus</button>
-      <button onClick={hndlMinus}>Minus</button>
-      <button onClick={hndlZe}>Zero</button>
+      <button onClick={()=>{setPluscnt(plusCount+1)}}>Plus</button>
+      <button onClick={()=>{setMincnt(minCount-1)}}>Minus</button>
+            <button onClick={()=>setZecnt(0)}>Zero</button>
 
     </div>
 
