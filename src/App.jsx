f266260df0d00f821325dbbf09f63332cc420b24
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-04-24 19:15:45 +0530

    Hooks-single UseState-Multi Object

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react"
 
 const App = () => {
 
-  const[plusCount,setPluscnt]=useState(0)
-  const[minCount,setMincnt]=useState(0)
-  const[zeCount,setZecnt]=useState(0)
+  const[count,setCount]=useState({
+    plus:0,
+    minus:0,
+    zero:0
+  })
+  // const[minCount,setMincnt]=useState(0)
+  // const[zeCount,setZecnt]=useState(0)
 
 
 
@@ -22,13 +26,19 @@ const App = () => {
 
   return (
     <div>
-      <div>Plus count: {plusCount}</div>
-      <div>Minus count: {minCount}</div>
-      <div>Zero count: {zeCount}</div>
+      <div>Plus count: {count.plus}</div>
+      <div>Minus count: {count.minus}</div>
+      <div>Zero count: {count.zero}</div>
     <div>
-      <button onClick={()=>{setPluscnt(plusCount+1)}}>Plus</button>
-      <button onClick={()=>{setMincnt(minCount-1)}}>Minus</button>
-            <button onClick={()=>setZecnt(0)}>Zero</button>
+      <button onClick={()=>{setCount({
+        ...count,plus:count+1
+      })}}>Plus</button>
+      <button onClick={()=>{setCount({
+        ...count,minus:count=1
+      })}}>Minus</button>
+            <button onClick={()=>setCount({
+              ...count,zero:0
+            })}>Zero</button>
 
     </div>
 
