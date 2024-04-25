ba447f78773ef0cb35df96223c3afd170568663b
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-04-25 14:49:08 +0530

    D7-Router Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import { useState } from "react"
+import {RouterProvider,createBrowserRouter} from "react-router-dom"
 
 const App = () => {
-
-  const[plusCount,setPluscnt]=useState(0)
-  const[minCount,setMincnt]=useState(0)
-  const[zeCount,setZecnt]=useState(0)
-
-
-
-//   const hndlPlus = () =>{
-//       setCounter(counter+1)
-//   }
-
-//   const hndlMinus = () =>{
-//     setCounter(counter-1)
-// } 
-
-// const hndlZe = () =>{
-//   setCounter(0)
-// }
-
-  return (
-    <div>
-      <div>Plus count: {plusCount}</div>
-      <div>Minus count: {minCount}</div>
-      <div>Zero count: {zeCount}</div>
-    <div>
-      <button onClick={()=>{setPluscnt(plusCount+1)}}>Plus</button>
-      <button onClick={()=>{setMincnt(minCount-1)}}>Minus</button>
-            <button onClick={()=>setZecnt(0)}>Zero</button>
-
-    </div>
-
-    </div>  )
+  const router = createBrowserRouter ({
+    path:"/",
+    element:<h1>Hello World</h1>
+  })
+  return <RouterProvider router={router} />
 }
 
 export default App
