225027fbfa3fea1fddaf9ba32b07e84ec4e73216
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-06-07 11:52:50 +0530

    Rendering mockAPI data with Axios-data verified in Postman

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,22 @@
 import { useLoaderData } from "react-router-dom"
+import axios from "axios"
 
 export async function loader (){
-const response = await fetch("https://666161f063e6a0189fe98846.mockapi.io/mytodo",
-    {
-        method : "GET",
-        headers : {
-            "Content-Type" : "application/json"
-        }
-    })
-    const todos = await response.json()
-    return { todos }
+    //to make the data available to the component using fetch()
+// const response = await fetch("https://666161f063e6a0189fe98846.mockapi.io/mytodo",
+//     {
+//         method : "GET",
+//         headers : {
+//             "Content-Type" : "application/json"
+//         }
+//     })
+//     const todos = await response.json()
+//     return { todos }
+// }
+
+    //to make the data available to the component using Axios
+    const todos = await axios.get('https://666161f063e6a0189fe98846.mockapi.io/mytodo')
+    return {todos:todos.data}
 }
 
 const Todos = () => {
